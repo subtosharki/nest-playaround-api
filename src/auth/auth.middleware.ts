a608f4a96c0c3a5c3c4901017aab0df6fcf79c84
subtosharki <camywam@yahoo,com>
2022-08-15 01:38:11 -0400

    fully working users route

--- src/auth/auth.middleware.ts
+++ src/auth/auth.middleware.ts
@@ -10,7 +10,7 @@ const AUTH_CODE = 'testcode';
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    if (req.query.token !== AUTH_CODE) throw new UnauthorizedException();
+    if (req.header('api-auth') !== AUTH_CODE) throw new UnauthorizedException();
     next();
   }
 }
