40a733801fb2cbbe050d3ada1ed51a4a3a122c7f
subtosharki <camywam@yahoo,com>
2022-08-15 20:17:47 -0400

    better handling

--- src/users/users.dto.ts
+++ src/users/users.dto.ts
@@ -1,8 +1,4 @@
-import {
-  IsBoolean,
-  IsNotEmpty,
-  IsString,
-} from 'class-validator';
+import { IsBoolean, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
@@ -12,36 +8,4 @@ export class CreateUserDto {
   @IsString()
   @IsNotEmpty()
   public password: string;
-}
-
-export class UpdateUsernameDto {
-  @IsString()
-  @IsNotEmpty()
-  public username: string;
-}
-
-export class UpdatePasswordDto {
-  @IsString()
-  @IsNotEmpty()
-  public password: string;
-}
-
-export class UpdateUserDto {
-  @IsString()
-  @IsNotEmpty()
-  public username: string;
-
-  @IsString()
-  @IsNotEmpty()
-  public password: string;
-
-  @IsBoolean()
-  @IsNotEmpty()
-  public admin: boolean;
-}
-
-export class UpdateAdminPermDto {
-  @IsBoolean()
-  @IsNotEmpty()
-  public admin: boolean;
 }
