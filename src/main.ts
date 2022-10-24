d413620e729c0bbdb6762870e2f10816593f81f0
subtosharki <camywam@yahoo.com>
2022-10-24 14:55:34 -0400

    validation pipe chaining

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableVersioning();
-  app.useGlobalPipes(new ValidationPipe());
+  app.enableVersioning().useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder()
     .setTitle('Sharki API')
     .setDescription('My API documentation')
